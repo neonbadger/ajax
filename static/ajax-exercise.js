97682d0aa5d6446dee2421a48a4df3644801844d
Shijie Feng <neonskylife@gmail.com>
2016-01-31 12:27:39 -0800

    use AJAX $.post method to get form info and change html elements

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -42,11 +42,40 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+// route /order-melons.json returns 
+// JSON 'code': result_code and 'msg': result_text
+
+function showOrderStatus(result) {
+    if (result.code === 'ERROR') {
+        $("#order-status").addClass("order-error");
+        $("#order-status").html('<p><b>'+result.msg+'</b></p>');
+    }
+    else {
+        $("#order-status").html('<p>'+result.msg+'</p>');
+    }
+
+    console.log(result.msg);
+    console.log(result.code);
+}
+
 function orderMelons(evt) {
     evt.preventDefault();
 
-    // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    // formInput: data for the .post method
+    // note no "action = '/...' " in html b/c that would submit 
+    // form data to a new url
+    // here, ajax collects the form data, sends to server
+    // server uses the form data and process them, generate 'code' and 'msg',
+    // sends back to AJAX as result, so AJAX can access result.code
+    // and result.msg.
+    // AJAX handles the page chanage, rather than HTML or Jinja, so page does not reload
+
+    var formInput = {
+        "melon_type": $("#melon-type-field").val(),
+        "qty": $("#qty-field").val()
+    };
+
+    $.post("/order-melons.json", formInput, showOrderStatus);
 }
 
 $("#order-form").on('submit', orderMelons);
